a03ac7fb1e08c1bf64d6395d7c062e06292f4371
Unknown <dr.nightingale@mail.ru>
2018-10-21 14:38:09 +0800

    add rss page

---
+++ mediadev.nenaprasno.ru/public/rss/index.php
@@ -0,0 +1,29 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle("Главная");
+?>
+<main class="main-content">
+    <?$APPLICATION->IncludeComponent("bitrix:rss.out","",Array(
+            "IBLOCK_TYPE" => "media",
+            "IBLOCK_ID" => MEDIA_ARTICLES_IBLOCK,
+            "SECTION_ID" => "",
+            "SECTION_CODE" => "",
+            "NUM_NEWS" => "",
+            "NUM_DAYS" => "",
+            "RSS_TTL" => "60",
+            "YANDEX" => "N",
+            "SORT_BY1" => "ACTIVE_FROM",
+            "SORT_ORDER1" => "DESC",
+            "SORT_BY2" => "SORT",
+            "SORT_ORDER2" => "ASC",
+            "FILTER_NAME" => "",
+            "CACHE_TYPE" => "A",
+            "CACHE_TIME" => "3600",
+            "CACHE_GROUPS" => "Y",
+            "CACHE_FILTER" => "N"
+        )
+    );?>
+</main>
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
